a85776e9b9af1fb06b9ca4a74f1d286effe7780a
tarukosu <4415085+tarukosu@users.noreply.github.com>
2020-12-14 13:45:24 +0900

    Add Quest sample project (#5)

---
+++ Unity/UniWasmQuestSample/Assets/UniWasm.MRTK/Scripts/GrabbableObject.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.MixedReality.Toolkit;
+using Microsoft.MixedReality.Toolkit.Input;
+using Microsoft.MixedReality.Toolkit.Utilities;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniWasm;
+
+public class GrabbableObject : MonoBehaviour
+{
+    Handedness attachedHandedness = Handedness.None;
+    WasmBehaviour wasm;
+
+    private CustomActionHandler grabHandler;
+    private CustomActionHandler useHandler;
+
+    public void Initialize(
+        WasmBehaviour wasm,
+        MixedRealityInputAction grabAction,
+        MixedRealityInputAction useAction)
+    {
+        this.wasm = wasm;
+
+        grabHandler = gameObject.AddComponent<CustomActionHandler>();
+        grabHandler.InputAction = grabAction;
+        grabHandler.SetFocusRequired(true);
+        grabHandler.OnInputActionStarted += GrabHandler_OnInputActionStarted;
+
+        useHandler = gameObject.AddComponent<CustomActionHandler>();
+        useHandler.InputAction = useAction;
+        // useHandler.SetFocusRequired(false);
+        useHandler.OnInputActionStarted += UseHandler_OnInputActionStarted;
+    }
+
+    private void UseHandler_OnInputActionStarted(BaseInputEventData e)
+    {
+        var inputEventData = e as InputEventData;
+        if (inputEventData == null)
+        {
+            return;
+        }
+
+        if (attachedHandedness != inputEventData.Handedness)
+        {
+            return;
+        }
+
+        wasm.InvokeOnTouchStart();
+
+    }
+
+    private void GrabHandler_OnInputActionStarted(BaseInputEventData e)
+    {
+        var inputEventData = e as InputEventData;
+        if (inputEventData == null)
+        {
+            return;
+        }
+
+        attachedHandedness = inputEventData.Handedness;
+
+        useHandler.SetFocusRequired(false);
+        Debug.Log(e.InputSource);
+    }
+
+    private void Update()
+    {
+        if (attachedHandedness == Handedness.None)
+        {
+            return;
+        }
+
+        foreach (var controller in CoreServices.InputSystem.DetectedControllers)
+        {
+            if (controller.ControllerHandedness != attachedHandedness)
+            {
+                continue;
+            }
+
+            var inputType = DeviceInputType.SpatialPointer;
+            foreach (MixedRealityInteractionMapping inputMapping in controller.Interactions)
+            {
+                if (inputMapping.InputType == inputType)
+                {
+                    var position = inputMapping.PositionData;
+                    var rotation = inputMapping.RotationData;
+
+                    transform.SetPositionAndRotation(position, rotation);
+                }
+            }
+        }
+    }
+}
---
+++ Unity/UniWasmQuestSample/Assets/UniWasm.MRTK/Scripts/LoaderForMRTK.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.MixedReality.Toolkit.Input;
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+using UnityEngine;
+
+namespace UniWasm.MRTK
+{
+    public class LoaderForMRTK : SemlLoader
+    {
+        [SerializeField]
+        private MixedRealityInputAction grabAction;
+
+        [SerializeField]
+        private MixedRealityInputAction useAction;
+
+        protected override WasmBehaviour AttatchScript(string path, XmlNode node, Transform parent)
+        {
+            var wasm = base.AttatchScript(path, node, parent);
+            if (wasm != null)
+            {
+                var grabbableObject = wasm.gameObject.AddComponent<GrabbableObject>();
+                grabbableObject.Initialize(wasm, grabAction, useAction);
+            }
+            return wasm;
+        }
+
+
+        protected override Transform InstantiatePrimitive(XmlNode node, Transform parent)
+        {
+            var t = base.InstantiatePrimitive(node, parent);
+            if (t != null)
+            {
+                var gameObject = t.gameObject;
+                // gameObject.AddComponent<ObjectManipulator>();
+                // gameObject.AddComponent<NearInteractionGrabbable>();
+                // var actionHandler = gameObject.AddComponent<GrabbableObject>();
+                // actionHandler.InputAction = new MixedRealityInputAction();
+            }
+
+            return t;
+        }
+    }
+}

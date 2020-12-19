6b86c287dad3e84be959052bd43d3495475cd926
Furuta Yusuke <furuta.yusuke.012@gmail.com>
2020-12-19 14:54:04 +0900

    load GLB

---
+++ Unity/UniWasmApp/Assets/UniWasm/Gltf/CustomInstantiator.cs
@@ -0,0 +1,20 @@
+﻿using GLTFast;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomInstantiator : GameObjectInstantiator
+{
+    public CustomInstantiator(Transform transform) : base(transform)
+    {
+
+    }
+
+    public override void AddPrimitive(uint nodeIndex, string meshName, Mesh mesh, Material[] materials, int[] joints = null, bool first = true)
+    {
+        base.AddPrimitive(nodeIndex, meshName, mesh, materials, joints, first);
+        var collider = nodes[nodeIndex].AddComponent<MeshCollider>();
+        collider.convex = true;
+    }
+}
+
---
+++ Unity/UniWasmApp/Assets/UniWasm/Gltf/GltfEntity.cs
@@ -0,0 +1,52 @@
+﻿using GLTFast;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class GltfEntity : GltfAssetBase
+{
+    /*
+    [Tooltip("URL to load the glTF from.")]
+    public string url;
+
+    [Tooltip("Automatically load at start.")]
+    public bool loadOnStartup = true;
+
+    [Tooltip("If checked, url is treated as relative StreamingAssets path.")]
+    public bool streamingAsset = false;
+    */
+
+    /*
+    protected virtual void Start()
+    {
+        Load(url)
+        if (loadOnStartup && !string.IsNullOrEmpty(url))
+        {
+            // Automatic load on startup
+            Load(
+                streamingAsset
+                    ? Path.Combine(Application.streamingAssetsPath, url)
+                    : url
+                );
+        }
+    }
+    */
+
+    /*
+    public void Load(string url)
+    {
+        Load(url);
+    }
+    */
+
+    protected override void OnLoadComplete(bool success)
+    {
+        if (success)
+        {
+            // Auto-Instantiate
+            gLTFastInstance.InstantiateGltf(new CustomInstantiator(transform));
+        }
+        base.OnLoadComplete(success);
+    }
+}
